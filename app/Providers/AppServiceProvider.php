968531c8270194b40b31d71a054839a341041a1f
Radan Stupar <radan.stupar@rcode.link>
2025-10-08 12:29:34 +0200

    Custom upload ruout, livewire

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,9 @@ final class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+
+        Livewire::setUpdateRoute(fn($handle) => Route::post('/livewire/update', $handle)
+            ->middleware(['web', 'auth']));
 
 
     }
